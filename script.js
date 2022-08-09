86f0cc490d623be1f626fb4c81af0efd742a0eda
paul serafim <paul.serafim.3021@gmail.com>
2022-08-09 12:30:00 +1000

    added code

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
-// http://www.omdbapi.com/?i=tt3896198&apikey=79968113
+// // http://www.omdbapi.com/?s=tt3896198&apikey=79968113
+// const movieContainer 
+// const searchBar
 
-async function main() {
-    const movies = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=79968113')
-    const moviesData = await movies.json()
-    console.log(
-    moviesData.map(
-        (movie) => `<div class="movie">
-        <div class="movie__container">
-            <h3>Movie Name</h3>
-            <img src="" alt="">
-        </div>
-        <div class="movie__summary">
-            <p></p>
-        </div>
-    </div>`
-    ).join('')
-    )
+const moviesContainer = document.querySelector(".movie__container")
+const searchBar = document.querySelector(".search__bar")
 
+async function getMovies(searchValue) {
+  const response = await fetch(`http://www.omdbapi.com/?s=${searchValue}&apikey=79968113`)
+  const data = await response.json()
+  data.Search.map((movie) => {
+    movieContainer.innerHTML += generateMovieHTML(movie)
+  })
 }
 
-main()
+
+getMovies()
+
+
+
+searchBar.addEventListener("keyup", (event) => {
+  if (event.code === "Enter") {
+      getMovies(event.target.value)
+  }
+})
+
+  data.Search.map((movie) => {
+    if (event.code === "Enter") {
+        getMovies(event.target.value)
+    }
+  })
+  
+
+  function generateMovieHtml(movie) {
+    return `<div class="movie">
+    <div class="movie__container">
+      <img src="${movie.Poster}" alt="">
+      <h3>${movie.Title}</h3>
+    </div>
+    <div class="movie__summary">
+        <p>${movie.Year}</p>
+        <p>${movie.ImdbID}</p>
+    </div>
+  </div>`
+}
