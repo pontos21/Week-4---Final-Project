a40844361f27cda7a4ae760907b5c1a1116f58b2
paul serafim <paul.serafim.3021@gmail.com>
2022-08-10 14:28:44 +1000

    code

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ async function getMovies(searchValue) {
 getMovies()
 
 function showMovie(id) {
-  localStorage.setItem("movie", id)
+  localStorage.setItem("id", id)
   window.location.href = `${window.location.origin}/user.html`
 }
 
